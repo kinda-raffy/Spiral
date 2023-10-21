5dc09ca15d5de30e3d72501a64b89b133d481d12
kinda_raffy <rafat.mahiuddin@gmail.com>
2023-10-22 03:12:44 +1100

    feat: add logging and timing calls

---
+++ Seperated/build_30_32/CMakeFiles/spiral.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spiral.dir/src/client.cpp.o"
+  "CMakeFiles/spiral.dir/src/constants.cpp.o"
+  "CMakeFiles/spiral.dir/src/core.cpp.o"
+  "CMakeFiles/spiral.dir/src/poly.cpp.o"
+  "CMakeFiles/spiral.dir/src/spiral.cpp.o"
+  "CMakeFiles/spiral.dir/src/testing.cpp.o"
+  "CMakeFiles/spiral.dir/src/util.cpp.o"
+  "spiral"
+  "spiral.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spiral.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
